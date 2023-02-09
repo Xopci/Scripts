e8dc13515ed03654e405f3b90775226537f8dd94
Xopic <3283304372@qq.com>
2023-02-10 01:53:35 +0800

    ks

--- QuantumultX/Task/ks/ksname.js
+++ QuantumultX/Task/ks/ksname.js
@@ -4,7 +4,7 @@ hostname = nebula.kuaishou.com
 
 [rewrite_local]
 ^https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview\/basicInfo? url script-response-body https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/Task/ks/ksname.js
-^https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview\/basicInfo? url script-response-body https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/Task/ks/ksExtraCookies.js
+^https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/(sign|box)\/(sign\?|explore.*?isOpen=true) url script-response-body https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/Task/ks/ksExtraCookies.js
 
 [task_local]
 0 30 7 * * ? https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/Task/ks/ksSign.js, tag=ks签到, enabled=true
