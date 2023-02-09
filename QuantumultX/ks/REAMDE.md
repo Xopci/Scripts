750efa61ea248cd93861cc1bfc3ce31d9427a09c
Xopic <3283304372@qq.com>
2023-02-09 20:45:08 +0800

    ks

---
+++ QuantumultX/ks/REAMDE.md
@@ -0,0 +1,12 @@
+------
+
+**ksExtraCookies套用了yijianyouni的模板，并修改了一下**
+
+**本人的基础不好，代码可能会有些低效或者bug**
+
+------
+
+## 感谢：
+
+[yijianyouni](https://github.com/yijianyouni)
+
--- QuantumultX/ks/ksname.js
+++ QuantumultX/ks/ksname.js
@@ -1,9 +1,20 @@
+/*
+[mitm]
 
-/**
- * @fileoverview Example to compose HTTP request
- * and handle the response.
- *
- */
+hostname = nebula.kuaishou.com
+
+[rewrite_local]
+^https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview\/basicInfo? -url script-response-body https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/ks/ksname.js
+^https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview\/basicInfo? -url script-response-body https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/ks/ksExtraCookies.js
+^https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview\/basicInfo? -url script-response-body 
+^https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview\/basicInfo? -url script-response-body 
+
+[task_local]
+
+0 30 7 * * ? https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/ks/ksSign.js, tag=ks签到, enabled=true
+0 0/20 * * * ? https://raw.githubusercontent.com/Xopci/Scripts/main/QuantumultX/ks/ksBox.js, tag=ks宝箱, enabled=true
+
+*/
 
 let obj = JSON.parse($response.body);
 let name = obj.data.userData.nickname;
